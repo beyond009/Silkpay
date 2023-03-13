7d3af70d7cdf87384d82d6c7d52354f47147fac2
byeond009 <1249413181@qq.com>
2023-03-13 23:05:10 +0800

    fix connect kit version

--- src/components/Web3Provider.tsx
+++ src/components/Web3Provider.tsx
@@ -1,15 +1,34 @@
 import { useTheme } from 'next-themes'
 import { APP_NAME } from '@/lib/consts'
-import { createClient, WagmiConfig } from 'wagmi'
+import { createClient, WagmiConfig, Chain, configureChains } from 'wagmi'
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc'
 import { ConnectKitProvider, getDefaultClient } from 'connectkit'
-
-const client = createClient(
-	getDefaultClient({
-		appName: APP_NAME,
-		autoConnect: true,
-		infuraId: process.env.NEXT_PUBLIC_INFURA_ID,
-	})
+const scrollTestNetwork: Chain = {
+	id: 534353,
+	name: 'Scroll Alpha Testnet',
+	network: 'Scroll Alpha Testnet',
+	nativeCurrency: { name: 'ETH', symbol: 'ETH', decimals: 18 },
+	rpcUrls: {
+		default: { http: ['https://alpha-rpc.scroll.io/l2'] },
+		public: { http: ['https://alpha-rpc.scroll.io/l2'] },
+	},
+	blockExplorers: {
+		default: { name: 'Scroll explorer', url: 'https://blockscout.scroll.io/' },
+	},
+	testnet: true,
+}
+const { chains, provider } = configureChains(
+	[scrollTestNetwork],
+	[
+		jsonRpcProvider({
+			rpc: chain => ({
+				http: `https://alpha-rpc.scroll.io/l2`,
+			}),
+		}),
+	]
 )
+
+const client = createClient(getDefaultClient({ appName: 'SilkPay', chains }))
 
 const Web3Provider = ({ children }) => {
 	const { resolvedTheme } = useTheme()
