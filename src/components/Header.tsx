ab8faf2ccf284d5097f06921144bcbab7a790036
byeond009 <1249413181@qq.com>
2023-03-18 15:36:38 +0800

    add logo

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ConnectWallet from '@/components/ConnectWallet'
+import { Logo } from './Logo'
+export const Header = () => {
+	return (
+		<div className="flex justify-between w-full max-w-6xl mx-auto py-12 sm:px-6 lg:px-8">
+			<div className="flex items-center text-4xl m-[-22px]">
+				<Logo /> Silkpay
+			</div>
+			<div className="flex">
+				<ConnectWallet />
+			</div>
+		</div>
+	)
+}
