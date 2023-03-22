915f014c1907c6373f2dc752a8b49855f8db74e9
byeond009 <1249413181@qq.com>
2023-03-22 12:49:45 +0800

    fix

--- src/pages/payment.tsx
+++ src/pages/payment.tsx
@@ -7,20 +7,6 @@ import ThemeSwitcher from '@/components/ThemeSwitcher'
 import { BackButton } from '@/components/BackButton'
 
 const Payment: FC = () => {
-	const [tabValue, setTabValue] = useState<number>(0)
-	const amountRef = useRef()
-	const oneRef = useRef()
-	const multiRef = useRef()
-	const handleCreate = () => {
-		const amount = (amountRef.current as HTMLInputElement).value
-		if (tabValue) {
-			const multi = (multiRef.current as HTMLTextAreaElement).value
-			let tempArray = multi.valueOf().split(/[\s,;:\t\r\n]+/)
-			console.log(tempArray)
-		} else {
-			const one = (oneRef.current as HTMLInputElement).value
-		}
-	}
 	return (
 		<div className="flex flex-col w-full h-full items-start justify-start max-w-6xl sm:pt-0">
 			<div className="flex">
